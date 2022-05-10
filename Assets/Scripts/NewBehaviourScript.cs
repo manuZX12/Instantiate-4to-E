485ccd93303f2c046a61ce90e443748b389fb050
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-05-10 15:27:10 -0300

    Insert Text

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Text txtUserName;
+
+    public void showgreeting()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+   
+}
